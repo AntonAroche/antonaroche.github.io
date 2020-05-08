a20caf13e5ce9420748d6637ee460c98dfee9209
Anton Aroche <anton.aroche@gmail.com>
2020-05-08 00:41:30 -0400

    Updates

---
+++ precache-manifest.e438172abe7ea110316498e9bd23eae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7355ce4250598088ce0b3bfb91d3ffc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f1271d841e19d9a2e0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4181244c855a54601d48",
+    "url": "/static/js/2.e4cac14e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4cac14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f1271d841e19d9a2e0",
+    "url": "/static/js/main.d8aa2f8d.chunk.js"
+  },
+  {
+    "revision": "8dd1ec03cd7bd48089e8",
+    "url": "/static/js/runtime-main.a5b7bec9.js"
+  }
+]);
